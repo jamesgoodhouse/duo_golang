2cc7357203749644e420e168f252a6234745c216
James J. Goodhouse <j.goodhouse@gmail.com>
2020-01-17 10:28:45 -0800

    got some tests in place

--- duoweb/duoweb.go
+++ duoweb/duoweb.go
@@ -6,6 +6,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"errors"
+	"log"
 	"strconv"
 	"strings"
 	"time"
@@ -24,10 +25,25 @@ const (
 	sKeyLen = 40
 )
 
+var (
+	defaultTimeNowFunc timeNowFunc = time.Now
+)
+
 type (
 	Duo interface {
 		SignRequest(*string) (*string, error)
 		VerifyResponse(*string) (*string, error)
+	}
+
+	Config struct {
+		ApplicationKey string `validate:"required,len=40"`
+		IntegrationKey string `validate:"required,len=20"`
+		SecretKey      string `validate:"required,len=40"`
+	}
+
+	SignRequestConfig struct {
+		Username string `validate:"required,excludes=0x7C"` // 0x7C == |
+		Config
 	}
 
 	timeNowFunc func() time.Time
@@ -58,7 +74,7 @@ func New(aKey, iKey, sKey string, opts ...option) (Duo, error) {
 		return nil, errors.New("invalid secret key")
 	}
 
-	d := &duoImpl{aKey, iKey, sKey, time.Now}
+	d := &duoImpl{aKey, iKey, sKey, defaultTimeNowFunc}
 
 	for _, opt := range opts {
 		opt(d)
@@ -74,14 +90,7 @@ func (d duoImpl) SignRequest(username *string) (*string, error) {
 		return nil, errors.New("username contains invalid character '|'")
 	}
 
-	vals := []string{*username, d.iKey}
-
-	duoSig := d.signValues(d.sKey, vals, duoPrefix, duoExpire)
-	appSig := d.signValues(d.aKey, vals, appPrefix, appExpire)
-
-	s := strings.Join([]string{duoSig, appSig}, ":")
-
-	return &s, nil
+	return d.signValues([]string{*username, d.iKey}), nil
 }
 
 func (d duoImpl) VerifyResponse(signedResp *string) (*string, error) {
@@ -100,15 +109,27 @@ func (d duoImpl) VerifyResponse(signedResp *string) (*string, error) {
 		return nil, err
 	}
 
-	if authUser != appUser {
-		return nil, errors.New("problem")
+	log.Printf("authUser: %s", *authUser)
+	log.Printf("appUser: %s", *appUser)
+
+	if *authUser != *appUser {
+		return nil, errors.New("auth user not equal to app user")
 	}
 
 	return authUser, nil
 }
 
-func (d duoImpl) signValues(key string, values []string, prefix string, expire time.Duration) string {
-	exp := d.timeNowFunc().Add(expire).Unix()
+func (d duoImpl) signValues(values []string) *string {
+	duoSig := signValues(d.sKey, duoPrefix, values, duoExpire, d.timeNowFunc)
+	appSig := signValues(d.aKey, appPrefix, values, appExpire, d.timeNowFunc)
+
+	s := strings.Join([]string{duoSig, appSig}, ":")
+
+	return &s
+}
+
+func signValues(key, prefix string, values []string, expire time.Duration, timeNow timeNowFunc) string {
+	exp := timeNow().Add(expire).Unix()
 
 	valuesWithExp := append(values, strconv.FormatInt(exp, 10))
 	joinedValues := strings.Join(valuesWithExp, "|")
@@ -125,18 +146,22 @@ func (d duoImpl) parseValues(key, val, prefix, ikey string) (*string, error) {
 	tNow := d.timeNowFunc().Unix()
 
 	uPrefix, uB64, uSig, err := splitSig(val)
+	log.Printf("uPrefix: %s", *uPrefix)
+	log.Printf("uB64: %s", *uB64)
+	log.Printf("uSig: %s", *uSig)
 	if err != nil {
 		return nil, err
 	}
 
 	sig := hmacSHA1(key, *uPrefix+"|"+*uB64)
+	log.Printf("sig: %s", sig)
 
 	if hmacSHA1(key, sig) != hmacSHA1(key, *uSig) {
-		return nil, errors.New("don't match")
+		return nil, errors.New("hmacSHA1 don't match")
 	}
 
 	if prefix != *uPrefix {
-		return nil, errors.New("don't match")
+		return nil, errors.New("prefix don't match")
 	}
 
 	cookie, err := base64.StdEncoding.DecodeString(*uB64)
@@ -174,11 +199,10 @@ func splitCookie(c string) (*string, *string, *int64, error) {
 		return nil, nil, nil, errors.New("problem")
 	}
 
-	t, err := time.Parse("", s[2])
+	ti, err := strconv.ParseInt(s[2], 10, 64)
 	if err != nil {
 		return nil, nil, nil, err
 	}
-	ti := t.Unix()
 
 	return &s[0], &s[1], &ti, err
 }
--- duoweb/duoweb_test.go
+++ duoweb/duoweb_test.go
@@ -1,7 +1,12 @@
 package duoweb
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/hex"
 	"errors"
+	"fmt"
 	"testing"
 	"time"
 
@@ -23,6 +28,9 @@ func TestNew(t *testing.T) {
 		{
 			description: "secret key invalid",
 		},
+		{
+			description: "time now function able to be overridden",
+		},
 	} {
 		t.Run(testCase.description, func(t *testing.T) {
 		})
@@ -31,17 +39,17 @@ func TestNew(t *testing.T) {
 
 func TestSignRequest(t *testing.T) {
 	for _, testCase := range []struct {
-		description   string
-		username      *string
-		timeNowFunc   func() time.Time
-		expected      *string
-		expectedError error
+		description     string
+		username        *string
+		timeNowOverride *time.Time
+		expected        *string
+		expectedError   error
 	}{
 		{
-			description: "returns a signed request",
-			username:    utils.StrToPtr("a username"),
-			timeNowFunc: func() time.Time { return time.Unix(1579051550, 0) },
-			expected:    utils.StrToPtr("TX|YSB1c2VybmFtZXwwMDAwMDAwMDAwMDAwMDAwMDAwMHwxNTc5MDUxODUw|4cb154be41f943a7be697d5a0f935f3115485fe0:APP|YSB1c2VybmFtZXwwMDAwMDAwMDAwMDAwMDAwMDAwMHwxNTc5MDU1MTUw|41d5bf2eeceade27c6a2726d1180fcd9cea2f1f6"),
+			description:     "returns a signed request",
+			username:        utils.StrToPtr("a username"),
+			timeNowOverride: func() *time.Time { t := time.Unix(1579051550, 0); return &t }(),
+			expected:        utils.StrToPtr("TX|YSB1c2VybmFtZXwwMDAwMDAwMDAwMDAwMDAwMDAwMHwxNTc5MDUxODUw|4cb154be41f943a7be697d5a0f935f3115485fe0:APP|YSB1c2VybmFtZXwwMDAwMDAwMDAwMDAwMDAwMDAwMHwxNTc5MDU1MTUw|41d5bf2eeceade27c6a2726d1180fcd9cea2f1f6"),
 		},
 		{
 			description:   "errors when username not set",
@@ -53,11 +61,18 @@ func TestSignRequest(t *testing.T) {
 			expectedError: errors.New("username contains invalid character '|'"),
 		},
 	} {
+		opts := []option{}
+
+		if testCase.timeNowOverride != nil {
+			f := SetTimeNowFunc(func() time.Time { return *testCase.timeNowOverride })
+			opts = append(opts, f)
+		}
+
 		d, err := New(
 			"0000000000000000000000000000000000000000",
 			"00000000000000000000",
 			"0000000000000000000000000000000000000000",
-			SetTimeNowFunc(testCase.timeNowFunc),
+			opts...,
 		)
 		if !assert.NoError(t, err) {
 			assert.FailNow(t, err.Error())
@@ -71,28 +86,66 @@ func TestSignRequest(t *testing.T) {
 	}
 }
 
-// func TestVerifyResponse(t *testing.T) {
-// 	for _, testCase := range []struct {
-// 		description   string
-// 		expectedError error
-// 	}{
-// 		{
-// 			description: "username not set",
-// 		},
-// 	} {
-// 		d, err := New(
-// 			"0000000000000000000000000000000000000000",
-// 			"00000000000000000000",
-// 			"0000000000000000000000000000000000000000",
-// 		)
-// 		if !assert.NoError(t, err) {
-// 			assert.FailNow(t, err.Error())
-// 		}
-// 		username := "a username"
-//
-// 		t.Run(testCase.description, func(t *testing.T) {
-// 			_, err := d.SignRequest(&username)
-// 			assert.Equal(t, testCase.expectedError, err)
-// 		})
-// 	}
-// }
+func TestVerifyResponse(t *testing.T) {
+	aKey := "AKEY_AKEY_AKEY_AKEY_AKEY_AKEY_AKEY_AKEY_"
+	iKey := "IKEY_IKEY_IKEY_IKEY_"
+	sKey := "SKEY_SKEY_SKEY_SKEY_SKEY_SKEY_SKEY_SKEY_"
+
+	timeNow := time.Now()
+
+	for _, testCase := range []struct {
+		description      string
+		authExpTime      int64
+		timeNowOverride  *time.Time
+		expectedError    error
+		expectedUsername *string
+	}{
+		{
+			description:      "returns a username",
+			timeNowOverride:  &timeNow,
+			authExpTime:      timeNow.Unix() + 1,
+			expectedUsername: utils.StrToPtr("tony_the_tiger"),
+		},
+		{
+			description:   "expired response",
+			authExpTime:   timeNow.Unix() - 1,
+			expectedError: errors.New("time is expired, buddy"),
+		},
+	} {
+		opts := []option{}
+
+		if testCase.timeNowOverride != nil {
+			f := SetTimeNowFunc(func() time.Time { return *testCase.timeNowOverride })
+			opts = append(opts, f)
+		}
+
+		d, err := New(aKey, iKey, sKey, opts...)
+		if !assert.NoError(t, err) {
+			assert.FailNow(t, err.Error())
+		}
+
+		username := "tony_the_tiger"
+
+		authB64 := base64.StdEncoding.EncodeToString([]byte(
+			fmt.Sprintf("%s|%s|%d", username, iKey, testCase.authExpTime),
+		))
+		h := hmac.New(sha1.New, []byte(sKey))
+		h.Write([]byte("AUTH|" + authB64))
+		authSig := hex.EncodeToString(h.Sum(nil))
+
+		appB64 := base64.StdEncoding.EncodeToString([]byte(
+			fmt.Sprintf("%s|%s|%d", username, iKey, timeNow.Unix()+3600),
+		))
+		h = hmac.New(sha1.New, []byte(aKey))
+		h.Write([]byte("APP|" + appB64))
+		appSig := hex.EncodeToString(h.Sum(nil))
+
+		signedResp := fmt.Sprintf("AUTH|%s|%s:APP|%s|%s", authB64, authSig, appB64, appSig)
+
+		t.Run(testCase.description, func(t *testing.T) {
+			u, err := d.VerifyResponse(&signedResp)
+			assert.Equal(t, testCase.expectedError, err)
+			assert.Equal(t, testCase.expectedUsername, u)
+		})
+	}
+}
